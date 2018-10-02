91b91312298b9b959927845fb7fa6fa6a6434028
Pablo Teruel <teruelpablo@gmail.com>
2018-10-02 16:41:38 -0300

    -Empty proyect.

---
+++ src/ar/edu/unlam/pb2/Subasta2/Subasta2.java
@@ -0,0 +1,5 @@
+package ar.edu.unlam.pb2.Subasta2;
+
+public class Subasta2 {
+
+}
---
+++ src/ar/edu/unlam/pb2/Subasta2Test/Subasta2Test.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.Subasta2Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Subasta2Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
