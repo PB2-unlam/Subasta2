9b5238e7e787a393d1a76989d67bde186838b911
Pablo Teruel <teruelpablo@gmail.com>
2018-10-02 17:56:04 -0300

    First commit

--- src/ar/edu/unlam/pb2/Subasta2/Subasta2.java
+++ src/ar/edu/unlam/pb2/Subasta2/Subasta2.java
@@ -1,5 +1,31 @@
 package ar.edu.unlam.pb2.Subasta2;
 
 public class Subasta2 {
+	private Integer mayorValor=0;
+	private Boolean estadoSubasta = true;
+	
+	//C
+	public Subasta2() {
+	
+	}
+	
+	//ofertar
+	public void ofertar(Integer valor) {
+		if(this.estadoSubasta) {
+			if(valor>this.mayorValor) {
+				this.mayorValor=valor;
+			}
+		}
+	}
 
+	//cerrar
+	public Integer cerrar() {
+		this.estadoSubasta=false;
+		return this.mayorValor;
+	}
+	
+	//estadoSubasta
+	public Boolean estadoSubasta() {
+		return this.estadoSubasta;
+	}
 }
--- src/ar/edu/unlam/pb2/Subasta2Test/Subasta2Test.java
+++ src/ar/edu/unlam/pb2/Subasta2Test/Subasta2Test.java
@@ -1,14 +1,30 @@
 package ar.edu.unlam.pb2.Subasta2Test;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import ar.edu.unlam.pb2.Subasta2.Subasta2;
 
 public class Subasta2Test {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void seCreaAbierta() {
+		Subasta2 seCreaAbierta = new Subasta2();
+		Boolean estado = seCreaAbierta.estadoSubasta();
+		Assert.assertTrue(estado);
+	}
+	
+	@Test
+	public void tieneQueCerrarConOfertaMasAlta() {
+		Subasta2 ofertaMasAlta = new Subasta2();
+		ofertaMasAlta.ofertar(123);
+		ofertaMasAlta.ofertar(12);
+		ofertaMasAlta.ofertar(434);
+		ofertaMasAlta.cerrar();
+		ofertaMasAlta.ofertar(345);
+		Integer actual = ofertaMasAlta.cerrar();
+		Integer expected = 434;
+		Assert.assertEquals(expected, actual);
 	}
 
 }
